2775f7efd00ed8368f5ba00c0cdafc515048f46e
MontuSherasiya <sherasiyamontu@gmail.com>
2026-05-24 13:58:35 +0530

    Update Vite and server configuration for development and production setup

--- server/index.js
+++ server/index.js
@@ -4,14 +4,25 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:5172",
+            "http://localhost:5173",
+            "http://localhost:3000",
+
+            //add production URL when deployed
+        ],
+        credentials: true
+    }
+));
 
 //API routes
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello from the server!' });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
